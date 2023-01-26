39976e250fb0af60a11627a928dbde949ee471a4
SaraDonzellini <saradonzie@gmail.com>
2023-01-26 15:23:02 +0100

    corrected warning at page start

--- partials/functions.php
+++ partials/functions.php
@@ -1,13 +1,23 @@
 <?php
 
-    function randomPassword()
-    {
-      $lettersAndSymbols = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()';
-      $pass = []; 
-      $alphaLength = strlen($lettersAndSymbols) - 1; 
-      for ($i = 0; $i < $_GET['userNumber']; $i++) {
-        $n = random_int(0, $alphaLength);
-        $pass[] = $lettersAndSymbols[$n];
-      }
-      return implode($pass); 
-    };
+
+
+function randomPassword()
+{
+  $lettersAndSymbols = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()';
+  $pass = [];
+
+  if (isset($_GET['userNumber'])) {
+    
+    $userNumber = $_GET['userNumber'];
+    
+    $alphaLength = strlen($lettersAndSymbols) - 1;
+    for ($i = 0; $i < $userNumber; $i++) {
+      $n = random_int(0, $alphaLength);
+      $pass[] = $lettersAndSymbols[$n];
+    }
+    return implode($pass);
+  }
+
+};
+
